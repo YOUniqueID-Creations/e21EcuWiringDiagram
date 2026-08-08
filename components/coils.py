a9ea761caa5c8287525c6fab2886af8b5f6ecdbf
Leo Zavala <1959214+dlz21@users.noreply.github.com>
2026-08-08 00:11:20 -0700

    Fix coils/routing

--- components/coils.py
+++ components/coils.py
@@ -5,16 +5,16 @@ def get_coil():
     return elm.Ic(
         pins=[
             elm.IcPin(name='ECU Trigger', pin='A', side='left'),
-            elm.IcPin(name='Logic GND', pin='B', side='left'),
-            elm.IcPin(name='Power Gnd', pin='C', side='left'),
+            elm.IcPin(name='Logic GND', pin='B', side='right'),
+            elm.IcPin(name='Power Gnd', pin='C', side='right'),
             elm.IcPin(name='12v', pin='D', side='left'),
         ],
-        w=1.5, h=2.0, pinspacing=0.8
+        w=5, h=2.5, pinspacing=1
     )
 
 def get_coil_1(mre_x, mre_y):
     """Returns a 4-pin LS1 coil."""
-    return get_coil().at((mre_x + 3, mre_y - 0)).label('Ignition\nCoil 1', loc='center')
+    return get_coil().at((mre_x + 3, mre_y)).label('Ignition\nCoil 1', loc='center')
 
 def get_coil_2(mre_x, mre_y):
     """Returns a 4-pin LS1 coil."""
--- routing.py
+++ routing.py
@@ -58,15 +58,14 @@ def connect_coil_1(d, mre, coil):
     Routes the ECU signal to the coil and grounds the coil.
     """
     # Wire Coil Pin A (ECU Trigger) to ECU Pin 9
-    d.add(elm.Line().left().at(coil.pinA).to(mre.pin9))
+    d.add(elm.Wire('|-').at(coil.pinA).to(mre.pin9))
 
     # Wire Coil Pin B (Logic GND) and C (Power GND)
-    # d.add(elm.Line().right().at(coil.pinB).length(0.5))
-    d.add(elm.Line().left().at(coil.pinC).length(0.5))
+    d.add(elm.Line().right().at(coil.pinC).length(0.2))
 
     # Grounds
-    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='bottom'))
-    d.add(elm.Line().down().to(engine_gnd.start))
+    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='right'))
+    d.add(elm.Line().right().to(engine_gnd.start))
 
 
     # Wire Coil Pin D (12v)
@@ -77,15 +76,15 @@ def connect_coil_2(d, mre, coil):
     Routes the ECU signal to the coil and grounds the coil.
     """
     # Wire Coil Pin A (ECU Trigger) to ECU Pin 10
-    d.add(elm.Line().left().at(coil.pinA).to(mre.pin10))
+    d.add(elm.Wire('|-').at(coil.pinA).to(mre.pin10))
 
     # Wire Coil Pin B (Logic GND) and C (Power GND)
     # d.add(elm.Line().right().at(coil.pinB).length(0.5))
-    d.add(elm.Line().down().at(coil.pinC).length(0.5))
+    d.add(elm.Line().right().at(coil.pinC).length(0.2))
 
     # Grounds
-    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='bottom'))
-    d.add(elm.Line().down().to(engine_gnd.start))
+    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='right'))
+    d.add(elm.Line().right().to(engine_gnd.start))
 
 
     # Wire Coil Pin D (12v)
@@ -96,15 +95,15 @@ def connect_coil_3(d, mre, coil):
     Routes the ECU signal to the coil and grounds the coil.
     """
     # Wire Coil Pin A (ECU Trigger) to ECU Pin 11
-    d.add(elm.Line().left().at(coil.pinA).to(mre.pin11))
+    d.add(elm.Wire('|-').at(coil.pinA).to(mre.pin11))
 
     # Wire Coil Pin B (Logic GND) and C (Power GND)
     # d.add(elm.Line().right().at(coil.pinB).length(0.5))
-    d.add(elm.Line().down().at(coil.pinC).length(0.5))
+    d.add(elm.Line().right().at(coil.pinC).length(0.2))
 
     # Grounds
-    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='bottom'))
-    d.add(elm.Line().down().to(engine_gnd.start))
+    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='right'))
+    d.add(elm.Line().right().to(engine_gnd.start))
 
 
     # Wire Coil Pin D (12v)
@@ -115,15 +114,14 @@ def connect_coil_4(d, mre, coil):
     Routes the ECU signal to the coil and grounds the coil.
     """
     # Wire Coil Pin A (ECU Trigger) to ECU Pin 12
-    d.add(elm.Line().left().at(coil.pinA).to(mre.pin12))
+    d.add(elm.Wire('|-').at(coil.pinA).to(mre.pin12))
 
     # Wire Coil Pin B (Logic GND) and C (Power GND)
-    # d.add(elm.Line().right().at(coil.pinB).length(0.5))
-    d.add(elm.Line().down().at(coil.pinC).length(0.5))
+    d.add(elm.Line().right().at(coil.pinC).length(0.2))
 
     # Grounds
-    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='bottom'))
-    d.add(elm.Line().down().to(engine_gnd.start))
+    engine_gnd = d.add(elm.Ground().label('Engine GND', loc='right'))
+    d.add(elm.Line().right().to(engine_gnd.start))
 
 
     # Wire Coil Pin D (12v)
