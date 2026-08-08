b796d248baab5d231debd28506b2b6ef1410641a
Leo Zavala <1959214+dlz21@users.noreply.github.com>
2026-08-07 22:37:32 -0700

    Add fan component

--- components/__init__.py
+++ components/__init__.py
@@ -1,4 +1,15 @@
 # components/__init__.py
+
 from .ecu import get_microrusefi_ecu
 from .wideband import get_rusefi_wbo
 from .crank import get_crank_sensor
+from .coils import *
+from .fan import *
+from .fuse_box import *
+from .ground_bus import *
+from .injectors import *
+from .maf import *
+from .relay_box import *
+from .stepper import *
+from .tps import *
+from .water_pump_tmp import *
---
+++ components/fan.py
@@ -0,0 +1,1 @@
+# TODO: Add fan and fan controller
